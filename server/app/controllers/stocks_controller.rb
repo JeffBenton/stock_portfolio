6e830524e8ff0b078d2837fdbafa9af0a0d97560
Jeff Benton <jeff@frogrock.net>
2020-02-14 23:11:52 -0700

    hooked up iex api for stock quotes

--- server/app/controllers/stocks_controller.rb
+++ server/app/controllers/stocks_controller.rb
@@ -1,6 +1,6 @@
 class StocksController < ApplicationController
   def buy
-    puts params
-    render json: true
+    res = HTTP.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=#{ENV['IEX_KEY']}").to_s
+    render json: res
   end
 end
