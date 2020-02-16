297f499433dc5384a90543057ef47e582bd64dfa
Jeff Benton <jeff@frogrock.net>
2020-02-15 21:58:10 -0700

    implemented more stock purchasing logic

--- server/app/controllers/stocks_controller.rb
+++ server/app/controllers/stocks_controller.rb
@@ -1,6 +1,10 @@
+require 'json'
+
 class StocksController < ApplicationController
   def buy
     res = HTTP.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=#{ENV['IEX_KEY']}").to_s
-    render json: res
+    data = JSON.parse(res)
+    # Stock.create(name: data["companyName"], ticker: data["symbol"], quantity: params["quantity"])
+    render json: { symbol: data["symbol"], open: data["open"], latestPrice: data["latestPrice"]}
   end
 end
