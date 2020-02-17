ce1d2a25f89bc06eac1312570b02b1d0e40128dd
Jeff Benton <jeff@frogrock.net>
2020-02-17 11:56:57 -0500

    commit for production deployment

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::API
   def authenticate(id, auth_token)
     User.find_by(id: id, auth_token: auth_token)
   end
+
+  def fallback_index_html
+    render :file => 'public/index.html'
+  end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+      !request.xhr? && request.format.html?
+    end
   scope '/api' do
     get '/users/:id', to: 'users#get'
     post '/signin', to: 'users#signin'
